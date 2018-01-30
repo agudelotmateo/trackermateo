21fa162f4f0bb28c255a5f58b0ed2db7ac7e3ee9
agudelotmateo <agudelotmateo@gmail.com>
2018-01-29 23:22:57 -0500

    Registration is now connected to the back end

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service'
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -43,7 +44,7 @@ const appRoutes: Routes = [
     FlashMessagesModule.forRoot()
   ],
   // Services
-  providers: [ValidateService],
+  providers: [ValidateService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service'
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-register',
@@ -14,7 +16,11 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService, private flashMessages: FlashMessagesService) { }
+  constructor(
+    private validateService: ValidateService,
+    private authService: AuthService,
+    private flashMessages: FlashMessagesService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -28,17 +34,27 @@ export class RegisterComponent implements OnInit {
     };
     console.log(user);
     if (!this.validateService.validateRegister(user)) {
-      this.flashMessages.show("Please fill in all fields", flashAlertSettings);
+      console.log("incomplete fields");
+      this.flashMessages.show("Please fill in all fields", { cssClass: "alert-danger", timeOut: 3000 });
       return false;
     }
     if (!this.validateService.validateEmail(user.email)) {
-      this.flashMessages.show("Please use a valid email address", flashAlertSettings);
+      console.log("invalid email");
+      this.flashMessages.show("Please use a valid email address", { cssClass: "alert-danger", timeOut: 3000 });
       return false;
     }
-  }
-}
 
-var flashAlertSettings = {
-  cssClass: 'alert-danger',
-  timeOut: 3000
+    // register user
+    this.authService.registerUser(user).subscribe(function (data) {
+      if (data.success) {
+        console.log("successfully registered");
+        this.flashMessages.show("You're now registered and can login", { cssClass: "alert-success", timeOut: 3000 });
+        this.router.navigate(["/login"]);
+      } else {
+        console.log("NOT successfully registered");
+        this.flashMessages.show("Registration was not succesful. Try again!", { cssClass: "alert-danger", timeOut: 3000 });
+        this.router.navigate(["/register"]);
+      }
+    });
+  }
 }
