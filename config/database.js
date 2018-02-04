301b17d6d159978d0dbb013b4e89a6c94e886fe2
agudelotmateo <agudelotmateo@gmail.com>
2018-02-04 00:37:02 -0500

    Change local DB to one from mLab

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     // `27017` is mongo's default port
     // `trackermateo` is the db's name
-    database: "mongodb://localhost:27017/trackermateo",
+    database: "mongodb://mateo:mateo@ds125068.mlab.com:25068/trackermateo",
     // the secret for our token
     secret: "secretirijillo"
 };
